dd711d097e9d2aadc5fad7434447be14d7073b7e
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-04-07 12:05:52 +0530

    backend - invoke api done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,39 @@
-import express from 'express'
-import runGraph from './ai/langGraph.ai.js'
-const app = express()
+import express from "express";
+import runGraph from "./ai/langGraph.ai.js";
+import cors from "cors";
+const app = express();
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from this origin
+    methods: ["GET", "POST"], // Allow these HTTP methods
+    credentials: true, // Allow cookies to be sent with requests
+  }),
+);
 
-app.get('/', async (req,res)=>{
-const result = await runGraph("how to become rich like top 1% people in the world")
-    res.json(result)
-})
+app.get("/", async (req, res) => {
+    res.status(200).json({ message: "Welcome to the AI Comparison API" });
+});
 
-export default app 
+app.post("/invoke", async (req, res) => {
+  const { problem } = req.body;
+  if (typeof problem !== "string") {
+    return res
+      .status(400)
+      .json({ error: "Problem query parameter must be a string" });
+  }
+  try {
+    const result = await runGraph(problem);
+    res
+      .status(201)
+      .json({ message: "Graph invoked successfully", success: true, result });
+  } catch (error) {
+    console.error("Error invoking graph:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing the request" });
+  }
+});
+
+export default app;
